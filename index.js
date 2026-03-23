0e7ee486523b0de6e60a8e1372c79c9fbb8ed144
Charlene-Lewis8 <charmarlew08@gmail.com>
2026-03-23 12:51:06 -0400

    js modal visibility

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-// template_oik7pgl template ID
-//service_j04r61g   service ID
-//NLOwO4CoICvXnJx_m  API public key
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_j04r61g',
+            'template_oik7pgl',
+            event.target,
+            'NLOwO4CoICvXnJx_m'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+        "The email service is temporarily unavailable. Please contact me directly at cmlewis777@gmail.com");
+    })
+}
