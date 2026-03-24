b3412e502e2cb143c255b3979cdfb8e97d3107b4
Charlene-Lewis8 <charmarlew08@gmail.com>
2026-03-23 23:50:26 -0400

    toggleContrast complete

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle){
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -19,7 +33,7 @@ function contact(event) {
     })
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
